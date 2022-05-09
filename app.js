823319bd4bd25166d1089c3e34cc0a04d351a053
Ho Huu Thanh <tridung.max@gmail.com>
2022-05-09 23:09:27 +0700

    tạo get/post đơn giản với expressjs

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let a = 0;
-let b = 10;
-console.log('compare a > b: ', a > b);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World. My name is John Doe!');
+});
+
+app.get('/cats', function(req, res) {
+    res.send(['Cat1', 'Cat2']);
+});
+
+app.post('/', function(req, res) {
+    res.send('Đây là kết quả của POST method');
+});
+var server = app.listen(8000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server running at http://%s:%s', host, port);
+});
